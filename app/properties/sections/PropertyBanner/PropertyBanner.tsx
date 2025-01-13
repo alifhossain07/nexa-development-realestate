e31bca0391c81ad6940716a8bb1c6937fd6a1e6b
Alif <alifhossain56782@gmail.com>
2025-01-13 15:08:28 +0600

    Property Page Banner Made Responsive

--- app/properties/sections/PropertyBanner/PropertyBanner.tsx
+++ app/properties/sections/PropertyBanner/PropertyBanner.tsx
@@ -1,18 +1,42 @@
 import React from 'react';
-import PropertyBannerImage from "@/public/images/propertyBanner.webp";
+import PropertyBannerImageDesktop from "@/public/images/propertyBanner.webp";
+import PropertyBannerImageMobile from "@/public/images/PropertyBannerMobile.webp";
+
 const PropertyBanner = () => {
-    return (
-        <div  className="relative bg-cover bg-center h-[900px]  flex items-start justify-center  transition-opacity duration-300"
+  return (
+    <div
+      className={`h-[600px]  flex items-center bg-cover bg-center transition-opacity duration-300`}
+      style={{
+        backgroundImage: `url(${PropertyBannerImageMobile.src})`,
+      }}
+    >
+      {/* Mobile Background */}
+      <div
+        className="absolute inset-0 block sm:hidden bg-cover bg-center"
         style={{
-          backgroundImage: `url(${PropertyBannerImage.src})`,
-          
-          
-          backgroundPosition: "center 40%", // 20% from the top, adjust as needed
-        }}>
-            
-        </div>
-    );
+          backgroundImage: `url(${PropertyBannerImageMobile.src})`,
+        }}
+      ></div>
+
+      {/* Desktop Background */}
+      <div
+        className="absolute inset-0 hidden sm:block bg-cover bg-center"
+        style={{
+          backgroundImage: `url(${PropertyBannerImageDesktop.src})`,
+        }}
+      ></div>
+
+      {/* Text Content */}
+      <div className="relative z-10 lg:ml-28 w-2/3 lg:mt-60 mt-80 lg:w-5/6  leading-10  px-4 sm:px-8">
+        <h1 className="text-white font-title uppercase text-2xl lg:text-6xl ">
+          Discover Nexa Skyline
+        </h1>
+        <p className="text-white font-title uppercase mt-8 tracking-wider text-lg lg:text-4xl ">
+          A New Standard in Premium <br className="hidden sm:block" /> Residential Living
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default PropertyBanner;
- 
